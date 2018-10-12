27f113dcc1eaa6b11f79fea02866c1ebcb268055
hukurou04 <shu.btc04@gmail.com>
2018-10-12 18:31:13 +0900

    photoのリスト化　完成 next inpomation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(function(){
+    $('.index-btn').click(function(){
+        var clickedIndex = $('.index-btn').index($(this));
+        $('.active').removeClass('active');
+        $('.photo').eq(clickedIndex).addClass('active');
+    });
 
 });
