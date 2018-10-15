6699400c1cd6be4b91395521edf8ba6f20e56ef9
hukurou04 <shu.btc04@gmail.com>
2018-10-16 02:56:59 +0900

    list 1.01

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 $(function(){
+
+    function toggleChangeBtn(){
+        var slideIndex = $('.slide').index($('.active'));
+        $('.change-btn').show();
+        if (slideIndex == 0) {
+            $('.prev-btn').hide();
+        }
+        else if (slideIndex == $('.slide').length - 1) {
+            $('.next-btn').hide();
+        }
+    }
+
     $('.index-btn').click(function(){
         var clickedIndex = $('.index-btn').index($(this));
         $('.active').removeClass('active');
-        $('.photo').eq(clickedIndex).addClass('active');
+        $('.slide').eq(clickedIndex).addClass('active');
+
+        toggleChangeBtn();
+
+    });
+
+    $('.change-btn').click(function(){
+        var $displaySlide = $('.active');
+        $displaySlide.removeClass('active');
+
+        if($(this).hasClass('next-btn')){
+            $displaySlide.next().addClass('active');
+        }
+        else{
+            $displaySlide.prev().addClass('active');
+        }
+
+        toggleChangeBtn();
     });
 
 });
